d1ef9bbdc6c02682d983e2a22dac5f0415e609f9
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-07 21:25:01 +0300

    added data in table

---
+++ GrafanaConfig/Models/ConfigLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafanaConfig.Models
+{
+    class ConfigLine : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public int Num { get; set; }
+        public int Status { get; set; }
+        public int Link { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public int E { get; set; }
+        public int F { get; set; }
+        public int G { get; set; }
+        public int H { get; set; }
+        public int I { get; set; }
+        public int K { get; set; }
+        public bool IsChanged { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
