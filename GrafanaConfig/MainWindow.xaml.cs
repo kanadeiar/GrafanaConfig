348a93ebdb69fafc7ecafb046e61ea9974ff9bf9
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-08 12:29:46 +0300

    delete nuget "fody"

--- GrafanaConfig/MainWindow.xaml.cs
+++ GrafanaConfig/MainWindow.xaml.cs
@@ -17,72 +17,72 @@ namespace GrafanaConfig
             #region Тестовые первоначальные данные
             configs.Add(new ConfigLine
             {
-                Name = "Novospasskoe", Num = 3, Status = 81686, Link = 81686, IsChanged = false,
+                Name = "Novospasskoe", Num = 3, Status = 81686, Link = 81686, //IsChanged = false,
                 A = 81689, B = 81686, C = 81666, D = 81690, E = 81691, F = 81665, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Novospasskoe", Num = 7, Status = 80367, Link = 80367, IsChanged = false,
+                Name = "Novospasskoe", Num = 7, Status = 80367, Link = 80367, //IsChanged = false,
                 A = 0, B = 80367, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Novospasskoe", Num = 20, Status = 40992, Link = 40992, IsChanged = false,
+                Name = "Novospasskoe", Num = 20, Status = 40992, Link = 40992, //IsChanged = false,
                 A = 40995, B = 40992, C = 40969, D = 58900, E = 58901, F = 40958, G = 39255, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Novospasskoe", Num = 21, Status = 81376, Link = 81376, IsChanged = false,
+                Name = "Novospasskoe", Num = 21, Status = 81376, Link = 81376, //IsChanged = false,
                 A = 81379, B = 81376, C = 81356, D = 81380, E = 81381, F = 81355, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 1, Status = 73508, Link = 73508, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 1, Status = 73508, Link = 73508, //IsChanged = false,
                 A = 0, B = 73508, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 2, Status = 408689, Link = 408689, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 2, Status = 408689, Link = 408689, //IsChanged = false,
                 A = 0, B = 408689, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 3, Status = 74223, Link = 74223, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 3, Status = 74223, Link = 74223, //IsChanged = false,
                 A = 0, B = 74223, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 8, Status = 73576, Link = 73576, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 8, Status = 73576, Link = 73576, //IsChanged = false,
                 A = 0, B = 73576, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 10, Status = 83805, Link = 83805, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 10, Status = 83805, Link = 83805, //IsChanged = false,
                 A = 83808, B = 83805, C = 83785, D = 83809, E = 83810, F = 83784, G = 0, H = 408340, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 11, Status = 41583, Link = 41583, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 11, Status = 41583, Link = 41583, //IsChanged = false,
                 A = 41586, B = 41583, C = 41560, D = 41895, E = 58896, F = 41559, G = 0, H = 413171, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 12, Status = 82223, Link = 82223, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 12, Status = 82223, Link = 82223, //IsChanged = false,
                 A = 82226, B = 82223, C = 82203, D = 82227, E = 82228, F = 82202, G = 82236, H = 82233, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 13, Status = 411527, Link = 411527, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 13, Status = 411527, Link = 411527, //IsChanged = false,
                 A = 411530, B = 411527, C = 411507, D = 411531, E = 411532, F = 411506, G = 413713, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 25, Status = 83490, Link = 83490, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 25, Status = 83490, Link = 83490, //IsChanged = false,
                 A = 0, B = 83490, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0, K = 0,
             });
             configs.Add(new ConfigLine
             {
-                Name = "Golodyaevskoe", Num = 26, Status = 62502, Link = 62502, IsChanged = false,
+                Name = "Golodyaevskoe", Num = 26, Status = 62502, Link = 62502, //IsChanged = false,
                 A = 62505, B = 62502, C = 62482, D = 62506, E = 62507, F = 62481, G = 62896, H = 62877, I = 0, K = 0,
             });
 
--- GrafanaConfig/Models/ConfigLine.cs
+++ GrafanaConfig/Models/ConfigLine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,21 +10,164 @@ namespace GrafanaConfig.Models
 {
     class ConfigLine : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public int Num { get; set; }
-        public int Status { get; set; }
-        public int Link { get; set; }
-        public int A { get; set; }
-        public int B { get; set; }
-        public int C { get; set; }
-        public int D { get; set; }
-        public int E { get; set; }
-        public int F { get; set; }
-        public int G { get; set; }
-        public int H { get; set; }
-        public int I { get; set; }
-        public int K { get; set; }
-        public bool IsChanged { get; set; }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value == name) return;
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private int num;
+        public int Num
+        {
+            get => num;
+            set
+            {
+                if (value == num) return;
+                num = value;
+                OnPropertyChanged(nameof(Num));
+            }
+        }
+        private int status;
+        public int Status
+        {
+            get => status;
+            set
+            {
+                if (value == status) return;
+                status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+        private int link;
+        public int Link
+        {
+            get => link;
+            set
+            {
+                if (value == link) return;
+                link = value;
+                OnPropertyChanged(nameof(Link));
+            }
+        }
+        private int a;
+        public int A
+        {
+            get => a;
+            set
+            {
+                if (value == a) return;
+                a = value;
+                OnPropertyChanged(nameof(A));
+            }
+        }
+        private int b;
+        public int B
+        {
+            get => b;
+            set
+            {
+                if (value == b) return;
+                b = value;
+                OnPropertyChanged(nameof(B));
+            }
+        }
+        private int c;
+        public int C
+        {
+            get => c;
+            set
+            {
+                if (value == c) return;
+                c = value;
+                OnPropertyChanged(nameof(C));
+            }
+        }
+        private int d;
+        public int D
+        {
+            get => d;
+            set
+            {
+                if (value == d) return;
+                d = value;
+                OnPropertyChanged(nameof(D));
+            }
+        }
+        private int e;
+        public int E
+        {
+            get => e;
+            set
+            {
+                if (value == e) return;
+                e = value;
+                OnPropertyChanged(nameof(E));
+            }
+        }
+        private int f;
+        public int F
+        {
+            get => f;
+            set
+            {
+                if (value == f) return;
+                f = value;
+                OnPropertyChanged(nameof(F));
+            }
+        }
+        private int g;
+        public int G
+        {
+            get => g;
+            set
+            {
+                if (value == g) return;
+                g = value;
+                OnPropertyChanged(nameof(G));
+            }
+        }
+        private int h;
+        public int H
+        {
+            get => h;
+            set
+            {
+                if (value == h) return;
+                h = value;
+                OnPropertyChanged(nameof(H));
+            }
+        }
+        private int i;
+        public int I
+        {
+            get => i;
+            set
+            {
+                if (value == i) return;
+                i = value;
+                OnPropertyChanged(nameof(I));
+            }
+        }
+        private int k;
+        public int K
+        {
+            get => k;
+            set
+            {
+                if (value == k) return;
+                k = value;
+                OnPropertyChanged(nameof(K));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
